b1a6b3edd3d3256101dc651ed5fdd4cddf5e302c
Antoine Karcher <antoine.karcher.id@gmail.com>
2023-03-05 03:16:45 +0100

    Added raise_run(), cleaned code

--- shell.py
+++ shell.py
@@ -4,12 +4,23 @@ import subprocess as sb
 class BaseShell:
     def __init__(self):
         pass
+    
+    def raise_run(self, stdin: sb._FILE = None, stdout: sb._FILE = None) -> sb.Popen:
+        o = self.run(stdin, stdout, stderr=sb.PIPE)
+        if o.returncode == 0:
+            return o
+        
+        args_str = " ".join(o.args)
+        err_str = o.stderr.read().decode("utf-8")
+        raise Exception(f"Command \"{args_str}\" with return code {o.returncode} and error: {err_str}")
 
-    def run(self, stdin: sb._FILE = None, stdout: sb._FILE = None, stderr: sb._FILE = None):
+
+    def run(self, stdin: sb._FILE = None, stdout: sb._FILE = None, stderr: sb._FILE = None) -> sb.Popen:
         pass
 
-    def pipe(self, shell: BaseShell):
-        pass
+    def pipe(self, shell: BaseShell) -> PipeShell:
+        return PipeShell(self, shell)
+
 
 class PipeShell(BaseShell):
     def __init__(self, shell1: BaseShell, shell2: BaseShell):
@@ -21,27 +32,12 @@ class PipeShell(BaseShell):
         p2 = self.shell2.run(stdin=p1.stdout, stdout=stdout, stderr=stderr)
         return p2
     
-    def pipe(self, shell: BaseShell):
-        return PipeShell(self, shell)
-
 class Shell(BaseShell):
     def __init__(self, cmd: str, args: list[str] = []):
         self.cmd    = cmd
         self.args   = args
     
     def run(self, stdin: sb._FILE = None, stdout: sb._FILE = None, stderr: sb._FILE = None):
-        self.args = " ".join( [self.cmd] + self.args)
-        self.args = ["-Command", self.args]
-        self.cmd = "powershell"
-
         out = sb.Popen([self.cmd] + self.args, stdin=stdin, stdout=stdout, stderr=stderr)
         out.wait()
         return out
-
-    def pipe(self, shell: BaseShell):
-        return PipeShell(self, shell)
-
-sh1 = Shell("echo", ['\'print(\"print(\\\"oui\\\")\")\''])
-sh2 = Shell("python")
-sh3 = Shell("python")
-sh1.pipe(sh2).pipe(sh3).run()
